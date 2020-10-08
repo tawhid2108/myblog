14ec3ef79a8e0488fe8a4ae26eded7e07b6c43ec
tawhid2108 <stawhid79@gmail.com>
2020-10-09 02:52:24 +0600

    First commit

---
+++ members/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import UserRegisterView, UserEditView
+
+
+
+
+urlpatterns = [
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('edit_profile/', UserEditView.as_view(), name='edit_profile'),
+
+]
---
+++ members/views.py
@@ -0,0 +1,26 @@
+from django.shortcuts import render
+from django.views import generic
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.urls  import reverse_lazy
+from .forms import SignUpForm
+
+
+
+class UserRegisterView(generic.CreateView):
+    form_class = SignUpForm
+    template_name = 'registration/register.html'
+    success_url = reverse_lazy('login')
+
+
+
+
+
+class UserEditView(generic.UpdateView):
+    form_class = UserChangeForm
+    template_name = 'registration/edit_profile.html'
+    success_url = reverse_lazy('login')
+
+
+
+    def get_object(self):
+        return self.request.user
